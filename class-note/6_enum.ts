88abd67248b09bb167d83d19c06f1227ea7e3883
jyr <jyr@wavers.co.kr>
2022-02-28 14:14:42 +0900

    이넘 활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -5,3 +5,30 @@ enum Shoes {
 
 var myShoes = Shoes.Nike;
 console.log(myShoes);
+
+// 예제
+// 이넘 활용 사례 (Dropdown 목록)
+enum Answer {
+    Yes = 'Y',
+    No = 'N',
+}
+
+// function askQuestion(answer: string) {
+function askQuestion(answer: Answer) {
+    // if (answer == 'yes') {
+    if (answer == Answer.Yes) {
+        console.log('정답입니다.');
+    }
+    // if (answer == 'no') {
+    if (answer == Answer.No) {
+        console.log('오답입니다.');
+    }
+}
+// askQuestion('yes');
+// askQuestion('예스');
+// askQuestion('y');
+// askQuestion('Yes');
+
+askQuestion(Answer.Yes);
+// TS2345: Argument of type '"Yes"' is not assignable to parameter of type 'Answer'.
+// askQuestion('Yes');
