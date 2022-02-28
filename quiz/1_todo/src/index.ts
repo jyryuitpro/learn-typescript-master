45efe88a9937c53ee03cfcfeccf5dfa6c3e59d77
jyr <jyr@wavers.co.kr>
2022-02-28 11:21:37 +0900

    실습 풀이 3 - 함수 구현

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -44,8 +44,19 @@ function showCompleted(): object[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = {
+    id: 4,
+    title: '아이템 4',
+    done: false,
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: '아이템 5',
+    done: false,
+  });
 }
 
 // NOTE: 유틸 함수
