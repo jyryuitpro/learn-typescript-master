c34970e98b47c3dc48928215e10d56130270739b
jyr <jyr@wavers.co.kr>
2022-02-28 12:37:55 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,4 +1,5 @@
 // function logMessage(value: string) {
+// function logMessage(value: any) {
 //     console.log(value);
 // }
 // logMessage('hello');
@@ -6,8 +7,17 @@
 // logMessage(100);
 
 // 유니온 타입(Union Type)
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
     console.log(value);
+    // 타입 가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
+    if (typeof value == 'number') {
+        value.toLocaleString()
+    }
+    if (typeof value == 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number');
 }
 logMessage('hello');
-logMessage(100)
+logMessage(100);
