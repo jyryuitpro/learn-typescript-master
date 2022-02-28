55c3f0ec1a78fdac045b48aad4f5c55e497db285
jyr <jyr@wavers.co.kr>
2022-02-28 11:49:01 +0900

    함수 구조를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -19,3 +19,17 @@ const capt = {
 };
 // 3_interface.ts(2, 5): 'age' is declared here.
 getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+var sum: SumFunction;
+sum = function(a: number, b: number): number {
+    return a + b;
+}
