94daa2e379ae6ac62abb2dd3daaf0e4f05e6fa3c
jyr <jyr@wavers.co.kr>
2022-02-28 15:18:03 +0900

    기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -7,8 +7,24 @@
 // logText(true);
 
 // 타입을 마치 함수의 파라미터의 개념으로 받게되는게 제네릭이라고 생각할 수 있습니다.
-function logText<T>(text: T): T {
+// function logText<T>(text: T): T {
+//     console.log(text);
+//     return text;
+// }
+// logText<string>('하이');
+
+function logText(text: string) {
     console.log(text);
+    // text.split('').reverse().join('');
     return text;
 }
-logText<string>('하이');
+
+function logNumber(num: number) {
+    console.log(num);
+    return num;
+}
+
+logText('a');
+// logText(10);
+const num = logNumber(10);
+logText(true);
