44996bf083c42472ae707b5f7b4ee9c736f1a56d
jyr <jyr@wavers.co.kr>
2022-02-28 11:02:15 +0900

    실습 풀이 2 - filter API & 화살표 함수 소개

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -30,12 +30,17 @@ function completeTodo(index: number, todo: object): void {
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
+  // return todoItems.filter(function (item) {
+  //   if (item.done) {
+  //     return item;
+  //   }
+  // });
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
@@ -44,7 +49,7 @@ function addTwoTodoItems() {
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
