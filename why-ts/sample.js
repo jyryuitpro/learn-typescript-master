a2add227a1b48159660f6a30bc98135182676ac3
jyr <jyr@wavers.co.kr>
2022-02-28 09:12:08 +0900

    자바스크립트를 타입스크립트처럼 코딩하는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+// var result = sum(10, 20);
+// result.toLocaleString();
+// sum(10, '20');
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
 function sum(a, b) {
     return a + b;
 }
-var result = sum(10, 20);
-result.toLocaleString();
-// sum(10, '20');
+sum(10, 20);
+sum(10, '20');
