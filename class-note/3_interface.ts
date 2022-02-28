f2557855f16a0921bb35edb8013e7564a72b00bb
jyr <jyr@wavers.co.kr>
2022-02-28 11:44:35 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 var seho: User = {
     age: 34,
     name: '세호'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user);
+}
+const capt = {
+    age: 100,
+    name: '캡틴'
+};
+// 3_interface.ts(2, 5): 'age' is declared here.
+getUser(capt);
