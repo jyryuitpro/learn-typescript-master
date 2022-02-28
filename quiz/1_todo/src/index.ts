87fc34d02972445e66efb4138d777e5ece128cbe
jyr <jyr@wavers.co.kr>
2022-02-28 11:32:40 +0900

    실습 풀이 4 - 오류 해결 및 구체적인 타입 정의

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,9 @@
-let todoItems: object[];
+// let todoItems: object[];
+let todoItems: { id: number; title: string; done: boolean }[];
 
 // api
-function fetchTodoItems(): object[] {
+// function fetchTodoItems(): object[] {
+function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -16,7 +18,9 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: object): void {
+// function addTodo(todo: object): void {
+function addTodo(todo: { id: number; title: string; done: boolean }): void {
+  // TS2345: Argument of type 'object' is not assignable to parameter of type '{ id: number; title: string; done: boolean; }'.
   todoItems.push(todo);
 }
 
@@ -24,7 +28,12 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: object): void {
+// function completeTodo(index: number, todo: object): void {
+function completeTodo(
+  index: number,
+  todo: { id: number; title: string; done: boolean }
+): void {
+  // TS2339: Property 'done' does not exist on type 'object'.
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
