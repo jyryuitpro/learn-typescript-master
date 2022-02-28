dac582312d4819277d168b749ffbf67a076a8d75
jyr <jyr@wavers.co.kr>
2022-02-28 10:18:09 +0900

    함수 타입 - 옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -20,3 +20,12 @@ function sum(a: number, b: number): number {
 sum(10, 20);
 // TS2554: Expected 2 arguments, but got 4.
 sum(10, 20, 30, 40);
+sum(10);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {
+
+}
+log('hello world');
+// log('hello ts', new Person());
+log('hello ts', 'abc');
