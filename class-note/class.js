e7a15d24bbf9bc50909643f750968465a207a289
jyr <jyr@wavers.co.kr>
2022-02-28 14:27:47 +0900

    자바스크립트 프로토타입 소개

--- class-note/class.js
+++ class-note/class.js
@@ -10,3 +10,19 @@ class Person {
 
 var seho = new Person('세호', 30);
 console.log(seho);
+
+// var user = { name: 'capt', age: 100 };
+// var admin = { name: 'capt', age: 100, role: 'admin' };
+
+var user = { name: 'capt', age: 100 };
+var admin = {};
+// __proto__의 상위에 user객체를 주겠다.
+admin.__proto__ = user;
+// user에서 가지고 있는 속성들을 내려받아서 재활용할 수 있습니다. (상속)
+admin.name;
+admin.age;
+
+// {}
+admin;
+admin.role = 'admin';
+
