9cd1964cf3a644f4b3e4876a2cc858e42f2b3dda
jyr <jyr@wavers.co.kr>
2022-02-28 10:09:07 +0900

    함수 타입 - 파라미터, 반환값

---
+++ class-note/2_functions.ts
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// 함수의 파라미터에 타입을 정의하는 방식
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+// sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    return 10;
+}
+
+// 함수의 타입을 정의하는 방식
+function sum(a: number, b: number): number {
+    return a + b;
+}
