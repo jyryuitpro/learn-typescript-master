8f8ec82edcc0e1080b480d43a3f30f766c9f78ec
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-01 09:40:42 +0900

    타입 단언 예제

--- class-note/10_type-assertions.ts
+++ class-note/10_type-assertions.ts
@@ -5,3 +5,9 @@ a = 'a';
 var b = a as string;
 
 // DOM API 조작
+// <div id="app">hi</div>
+
+var div = document.querySelector('div');
+if (div) {
+    div.innerText
+}
--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -107,6 +107,11 @@ class AddressBook {
   /* ------------------------------------------------ */
 }
 
+var div = document.querySelector('.container') as HTMLDivElement;
+if (div) {
+  div.innerText;
+}
+
 let heroes = [
   { name: 'Tony', age: 30 },
   { name: 'Captain', age: 100 },
