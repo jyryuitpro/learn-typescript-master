5ae675954293e231b547cbd98795cd8e87e7664e
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-01 09:48:03 +0900

    타입 가드를 위한 예제 소개

---
+++ class-note/11_type-guard.ts
@@ -0,0 +1,24 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function introduce(): Developer | Person {
+    return { name: 'Tony', age: 33, skill: 'Iron Making'};
+}
+var tony = introduce();
+// console.log(tony.skill);
+console.log(tony.name);
+
+if ((tony as Developer).skill) {
+    var skill = (tony as Developer).skill;
+    console.log(skill);
+} else if ((tony as Person).age) {
+    var age = (tony as Person).age;
+    console.log(age);
+}
