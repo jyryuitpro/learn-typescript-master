c34f601047d2ea4fb4ffdd4adf6f0c65495d00f4
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-01 09:23:21 +0900

    복잡한 구조에서의 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,11 +7,31 @@ function getB(b = 10) {
 }
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
     value: T;
     title: string;
 }
-var shoppingItem: Dropdown<string> = {
-    value: 'abc',
-    title: 'hello'
+interface DetailedDropdown<K> extends Dropdown<K> {
+    description: string;
+    tag: K;
+    // value: T;
+    // title: string;
 }
+
+var detailedItem: DetailedDropdown<string> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'a',
+    tag: 'a'
+}
+
