632006bf5cf107c1dcd8246214207b0db1ba702d
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-01 14:43:37 +0900

    맵드 타입 예제

---
+++ class-note/14_mapped-type.ts
@@ -0,0 +1,15 @@
+type Heroes = 'Hulk' | 'Capt' | 'Thor'
+// Initial type:
+// {Hulk: number, Capt: number, Thor: number}
+type HeroAges = { [K in Heroes]: number }
+const ages: HeroAges = {
+    Hulk: 33,
+    Capt: 100,
+    Thor: 1000
+}
+
+// for in 반복문 코드
+var arr = ['a', 'b', 'c'];
+for (var key in arr) {
+    console.log(arr[key]);
+}
