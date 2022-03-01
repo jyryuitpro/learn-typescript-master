c8dfc741e64fcc154a57760b6674a00a9213c3d5
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-01 10:11:18 +0900

    타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -20,3 +20,31 @@ person = developer;
 
 // TS2741: Property 'skill' is missing in type 'Person' but required in type 'Developer'.
 // developer = new Person();
+
+// 함수
+var add = function(a: number) {
+    // ...
+}
+var sum = function(a: number, b: number) {
+    // ...
+}
+// TS2322: Type '(a: number, b: number) => void' is not assignable to type '(a: number) => void'.
+// add = sum;
+sum = add;
+
+// 제네릭
+interface Empty<T> {
+
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+    data: T;
+}
+var notempty1: NotEmpty<string>;
+var notempty2: NotEmpty<number>;
+notempty1 = notempty2;
+notempty2 = notempty1;
