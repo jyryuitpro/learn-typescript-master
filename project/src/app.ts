a72e9f3a7ff6cf43de64716b34dfea9c912f2fbe
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-19 16:12:27 +0900

    API 함수 타입 정의에 따른 타입 오류 해결

--- project/src/app.ts
+++ project/src/app.ts
@@ -83,14 +83,14 @@ async function handleListClick(event: any) {
   clearRecoveredList();
   startLoadingAnimation();
   isDeathLoading = true;
-  const { data: deathResponse } = await fetchCountryInfo(selectedId, 'deaths');
+  const { data: deathResponse } = await fetchCountryInfo(selectedId, CovidStatus.Deaths);
   const { data: recoveredResponse } = await fetchCountryInfo(
     selectedId,
-    'recovered',
+    CovidStatus.Recovered,
   );
   const { data: confirmedResponse } = await fetchCountryInfo(
     selectedId,
-    'confirmed',
+    CovidStatus.Confirmed,
   );
   endLoadingAnimation();
   setDeathsList(deathResponse);
