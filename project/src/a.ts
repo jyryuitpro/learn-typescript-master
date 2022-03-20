2569ac3f1aefb5e639ef8250e0d0e9ab61a76306
jyr <jyr@wavers.co.kr>
2022-03-20 16:43:59 +0900

    타입 단언 문법 사용시 주의해야 할 점

---
+++ project/src/a.ts
@@ -0,0 +1,20 @@
+interface Hero {
+  name: string;
+  skill: string;
+}
+
+// const capt: Hero = {
+//   name: 'capt',
+//   skill: 'shield',
+// };
+
+// const capt: Hero = {};
+// const capt = {} as Hero;
+// capt.name = 'capt';
+// capt.skill = 'shield';
+
+const a: string | null;
+// a!
+// if (a) {
+//   a
+// }
